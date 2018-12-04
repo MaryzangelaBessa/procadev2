13b0f95840ceccf888eddf09e6121497b35885aa
Abilio Castro <castro.abilio@gmail.com>
2018-12-04 13:16:45 -0300

    Adicionando mais elementos na interface

--- ProjetoFinalPDS/src/casas/SorteReves.java
+++ ProjetoFinalPDS/src/casas/SorteReves.java
@@ -4,19 +4,28 @@ import java.util.ArrayList;
 
 import JGamePlay.GameImage;
 import base.Jogador;
+import base.JogoMain;
 import util.Posicao;
 
 public class SorteReves extends CasaTabuleiro {
 
 	public SorteReves(Posicao posicao) {
 		super(posicao);
-		// TODO Auto-generated constructor stub
+		this.imagem = new GameImage(JogoMain.filepath + "assets/setback.png");
+		this.imagem.setDimension(25, 38);
+		this.imagem.x = posicao.x;
+		this.imagem.y = posicao.y;
 	}
 
 	@Override
 	public void addJogador(Jogador jogador) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@Override
+	public void desenhar() {
+		super.desenhar();
 	}
 
 }
