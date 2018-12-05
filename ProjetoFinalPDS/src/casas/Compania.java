e96b5edeacaa097f06ce81a347b2e2d6b3e616e4
Abilio Castro <castro.abilio@gmail.com>
2018-12-05 14:15:11 -0300

    Terreno Menu

--- ProjetoFinalPDS/src/casas/Compania.java
+++ ProjetoFinalPDS/src/casas/Compania.java
@@ -22,7 +22,37 @@ public class Compania extends CasaTabuleiro {
 		this.imagem.setDimension(105, 114);
 		this.imagem.x = posicao.x;
 		this.imagem.y = posicao.y;
+		this.setLugares();
 
+	}
+	
+	private void setLugares() {
+		Posicao p1 = new Posicao(this.imagem.x, this.imagem.y + 35);
+		Posicao p2 = new Posicao(this.imagem.x, this.imagem.y + 62);
+		Posicao p3 = new Posicao(this.imagem.x, this.imagem.y + 87);
+		Posicao p4 = new Posicao(this.imagem.x + 95, this.imagem.y + 35);
+		Posicao p5 = new Posicao(this.imagem.x + 95, this.imagem.y + 62);
+		Posicao p6 = new Posicao(this.imagem.x + 95, this.imagem.y + 87);
+		this.lugaresJogadores.add(p1);
+		this.lugaresJogadores.add(p2);
+		this.lugaresJogadores.add(p3);
+		this.lugaresJogadores.add(p4);
+		this.lugaresJogadores.add(p5);
+		this.lugaresJogadores.add(p6);
+	}
+	
+	@Override
+	public void addJogador(Jogador jogador) {
+		jogador.setPosicaoAtual(this);
+		for (int i = 0; i < lugaresJogadores.size(); i++) {
+			if(this.lugaresOcupados[i] == null) {
+				this.lugaresOcupados[i] = jogador;
+				jogador.getPersonagem().x = this.lugaresJogadores.get(i).x;
+				jogador.getPersonagem().y = this.lugaresJogadores.get(i).y;
+				break;
+			}
+		}
+		
 	}
 
 	public void comprarCompania(Jogador novoProprietario) {
@@ -76,12 +106,6 @@ public class Compania extends CasaTabuleiro {
 
 	public void setPrecoCompania(int precoCompania) {
 		this.precoCompania = precoCompania;
-	}
-
-	@Override
-	public void addJogador(Jogador jogador) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
--- ProjetoFinalPDS/src/casas/SorteReves.java
+++ ProjetoFinalPDS/src/casas/SorteReves.java
@@ -20,12 +20,36 @@ public class SorteReves extends CasaTabuleiro {
 		this.imagem.setDimension(25, 38);
 		this.imagem.x = posicao.x;
 		this.imagem.y = posicao.y;
+		this.setLugares();
+	}
+	
+	private void setLugares() {
+		Posicao p1 = new Posicao(this.imagem.x, this.imagem.y + 35);
+		Posicao p2 = new Posicao(this.imagem.x, this.imagem.y + 62);
+		Posicao p3 = new Posicao(this.imagem.x, this.imagem.y + 87);
+		Posicao p4 = new Posicao(this.imagem.x + 95, this.imagem.y + 35);
+		Posicao p5 = new Posicao(this.imagem.x + 95, this.imagem.y + 62);
+		Posicao p6 = new Posicao(this.imagem.x + 95, this.imagem.y + 87);
+		this.lugaresJogadores.add(p1);
+		this.lugaresJogadores.add(p2);
+		this.lugaresJogadores.add(p3);
+		this.lugaresJogadores.add(p4);
+		this.lugaresJogadores.add(p5);
+		this.lugaresJogadores.add(p6);
 	}
 
 	@Override
 	public void addJogador(Jogador jogador) {
-		// TODO Auto-generated method stub
-
+		jogador.setPosicaoAtual(this);
+		for (int i = 0; i < lugaresJogadores.size(); i++) {
+			if(this.lugaresOcupados[i] == null) {
+				this.lugaresOcupados[i] = jogador;
+				jogador.getPersonagem().x = this.lugaresJogadores.get(i).x;
+				jogador.getPersonagem().y = this.lugaresJogadores.get(i).y;
+				break;
+			}
+		}
+		
 	}
 
 	@Override
